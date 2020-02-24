2da842fff58b4c5c40698f492a7176da446938cc
choiseyeoni <choiseyeoni@gmail.com>
2020-02-24 21:02:53 +0900

    Connecting to MongoDB, Configuring dotenv

--- db.js
+++ db.js
@@ -1,67 +1,18 @@
-export const videos = [
-  {
-    id: 4234243,
-    title: "Video awesome",
-    description: "This is something i love",
-    views: 29749,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3928,
-      name: "yeoni",
-      email: "yeoni@sy.com"
-    }
-  },
-  {
-    id: 294283,
-    title: "Sample video",
-    description: "wowowowowow That's great!",
-    views: 124,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3928,
-      name: "yeoni",
-      email: "yeoni@sy.com"
-    }
-  },
-  {
-    id: 78587,
-    title: "amazing... What is this?",
-    description: "gooooooooood.",
-    views: 103839,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3928,
-      name: "yeoni",
-      email: "yeoni@sy.com"
-    }
-  },
-  {
-    id: 2394,
-    title: "who are u......n_n",
-    description: "fake this",
-    views: 72494,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3928,
-      name: "yeoni",
-      email: "yeoni@sy.com"
-    }
-  },
-  {
-    id: 31098,
-    title: "love is powerful💛",
-    description: "im happy🥰",
-    views: 225490,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3928,
-      name: "yeoni",
-      email: "yeoni@sy.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error =>
+  console.log(`❌  Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
