b2f3c9f80629165cdf5f4dd42a7f5d84a7618b9d
choiseyeoni <choiseyeoni@gmail.com>
2020-02-26 02:56:59 +0900

    Comment Model, Home Controller finish

--- init.js
+++ init.js
@@ -2,7 +2,8 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
-import "./models/Video"; // 이렇게 model을 import 해 줌으로써 이 파일을 실행시키면 model을 생성했다는 걸 mongoose가 인지해서 그걸 model로써 등록하게 한다.
+import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000;
 
--- models/Video.js
+++ models/Video.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const VideoSchema = new mongoose.Schema({
   fileUrl: {
     type: String,
-    // fileUrl 값이 없는 비디오를 생성하려 한다면 이 에러 메시지를 받을 것임
     required: "File URL is required"
   },
   title: {
@@ -13,14 +12,19 @@ const VideoSchema = new mongoose.Schema({
   description: String,
   views: {
     type: Number,
-    default: 0 // 처음 비디오가 생성되면 views가 0이 되게 함
+    default: 0
   },
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
-// model의 이름 : Video, Video model의 schema : VideoSchema
+
 const model = mongoose.model("Video", VideoSchema);
-// export default가 변수, 함수, 오브젝트, 클래스 등을 보낼 수 있는 명령어라는데, 내가 니꼬쌤한테 배운 거랑은 좀 다르구나. 진짜 파일 전체를 내보내는 게 맞나 이게?
 export default model;
